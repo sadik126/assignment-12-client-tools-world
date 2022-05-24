01814ccbdd7a56f679ffc9f660aa85e87716c730
sadik126 <sadikhimel04@gmail.com>
2022-05-24 20:48:29 +0600

    m

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Purchase from './Components/Purchase/Purchase';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
+      </Routes>
 
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "red",
+          secondary: "#f6d860",
+          accent: "#a991f7",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+
+      "cupcake",
+    ],
+  },
   theme: {
     extend: {},
   },
