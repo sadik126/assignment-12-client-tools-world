4717a69727bed25235e30ee619ada4eb933114f4
sadik126 <sadikhimel04@gmail.com>
2022-05-25 14:13:00 +0600

    m

--- src/Components/Tools/Tools.js
+++ src/Components/Tools/Tools.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
 
 const Tools = () => {
-    const [tool, setTool] = useState([])
+    const [tools, setTools] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:4000/tools')
             .then(res => res.json())
-            .then(data => setTool(data))
+            .then(data => setTools(data))
     }, [])
 
 
     return (
         <div>
+
+            <div className="container">
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+                    {
+                        tools.map(tool => <Tool key={tool._id} tool={tool}></Tool>)
+                    }
+
+                </div>
+            </div>
 
         </div>
     );
